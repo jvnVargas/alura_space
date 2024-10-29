f97979ffc34680e51051f41d8012b2e3adaf348b
J_Vargas <vargas.jvn02@gmail.com>
2024-10-29 12:36:43 -0300

    aula 5 completa

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id","nome")
-    list_editable = ("publicada",)
     search_fields = ("nome",)
+    list_filter = ("categoria","usuario",)
+    list_editable = ("publicada",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
